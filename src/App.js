18f726e4e66d29a9db056ea16e4bc4da6fdaea7b
Omwansam <omwansamarnold@gmail.com>
2024-08-21 04:31:23 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+  <h1>Hi, I'm </h1>
+  <h2>About Me</h2>
+  <p>I am pursuing a career in Software Development. I am very passionate about the work and am eager to learn as much as I can</p>
+  
+ <div> <img src="https://i.pinimg.com/564x/b3/f8/a9/b3f8a9ffb414e67cc998f6a5ae244dcd.jpg" alt="Picture" />
+  </div>
+  <a name="linkedIn" href="https://www.linkedin.com/feed/">LinkedIn</a>
+  <div></div>
+  <a name="github" href="https://github.com/">GitHub</a>
+</div>;
 }
 
 export default App;
